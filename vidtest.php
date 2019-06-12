f0cfb7e0b74bad476c43dbf2d8cd5fa48d924803
hasan <hakbari36@yahoo.co.id>
2019-06-12 14:26:59 +0700

    Add files via upload

---
+++ vidtest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>untitled</title>
+	<meta charset="utf-8" />
+	<meta name="generator" content="Geany 1.32" />
+</head>
+<body>
+	<video controls autoplay width="320" height="640">
+		<source src="vid[00].mp4" type="video/mp4"/>
+	</video>
+</body>
+</html>
